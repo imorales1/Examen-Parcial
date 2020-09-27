83b473754d4d3d9df67db92353a3ac31782991f8
Alejandro Morales <alejan_dro-morales@hotmail.com>
2020-09-26 21:19:00 -0600

    Primer Commit

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+
+        private SqlConnection Conec = new SqlConnection(ConfigurationManager.ConnectionStrings["cadena"].ConnectionString);
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conec.State == ConnectionState.Closed)
+                Conec.Open();
+            return Conec;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conec.State == ConnectionState.Open)
+                Conec.Close();
+            return Conec;
+        }
+
+    }
+}
